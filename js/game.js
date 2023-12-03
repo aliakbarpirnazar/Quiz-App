c4d53cd6da73529a0eecdf2302663064520990b9
aliakbarpirnazar <aliakbarpirnazar@gmail.com>
2023-12-03 13:19:58 +0330

    final Update

--- js/game.js
+++ js/game.js
@@ -1,20 +1,35 @@
 import formatData from "./helper.js";
 
+const level = localStorage.getItem("level") || "easy";
+
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 const questionText = document.getElementById("question-text");
 const answerList = document.querySelectorAll(".answer-text");
-const URL =
-  "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple";
+const scoreText = document.getElementById("score");
+const nextButton = document.getElementById("next-button");
+const questionNumber = document.getElementById("question-number");
+const finishButton = document.getElementById("finish-button");
+const error = document.getElementById("error");
+
+const COREECT_BONUS = 10;
+const URL = `https://opentdb.com/api.php?amount=10&difficulty=${level}&type=multiple`;
 let formattedData = null;
 let questionIndex = 0;
 let correctAnswer = null;
+let score = 0;
+let isAccepted = true;
 
 const fetchData = async () => {
-  const response = await fetch(URL);
-  const json = await response.json();
-  formattedData = formatData(json.results);
-  start();
+  try {
+    const response = await fetch(URL);
+    const json = await response.json();
+    formattedData = formatData(json.results);
+    start();
+  } catch (err) {
+    loader.style.display = "none";
+    error.style.display = "block";
+  }
 };
 
 const start = () => {
@@ -24,19 +39,54 @@ const start = () => {
 };
 
 const showQuestion = () => {
+  questionNumber.innerText = questionIndex + 1;
   const { question, answers, correctAnswerIndex } =
     formattedData[questionIndex];
-  console.log(answerList, answers);
   correctAnswer = correctAnswerIndex;
+  // console.log(correctAnswer);
   questionText.innerText = question;
   answerList.forEach((button, index) => {
     button.innerText = answers[index];
   });
 };
 
-const checkAnswer = () => {};
+const checkAnswer = (event, index) => {
+  if (!isAccepted) return;
+  isAccepted = false;
+  const isCorrect = index === correctAnswer ? true : false;
+  if (isCorrect) {
+    event.target.classList.add("correct");
+    score += COREECT_BONUS;
+    scoreText.innerText = score;
+  } else {
+    event.target.classList.add("incorrect");
+    answerList[correctAnswer].classList.add("correct");
+  }
+};
+
+const nextHandler = () => {
+  questionIndex++;
+  if (questionIndex < formattedData.length) {
+    isAccepted = true;
+    removeClasses();
+    showQuestion();
+  } else {
+    finishHandler();
+  }
+};
+
+const finishHandler = () => {
+  localStorage.setItem("score", JSON.stringify(score));
+  window.location.assign("/end.html");
+};
+
+const removeClasses = () => {
+  answerList.forEach((button) => (button.className = "answer-text"));
+};
 
 window.addEventListener("load", fetchData);
+nextButton.addEventListener("click", nextHandler);
+finishButton.addEventListener("click", finishHandler);
 answerList.forEach((button, index) => {
-  button.addEventListener("click", checkAnswer);
+  button.addEventListener("click", (event) => checkAnswer(event, index));
 });
